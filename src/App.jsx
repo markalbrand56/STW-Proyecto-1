17e0ed6e8894f21b3d851af364b8413e7e89d48b
Mark Albrand <mark.albrand5@gmail.com>
2023-04-27 16:54:29 -0600

    calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import PlatformNav from "./components/Platform Nav/PlatformNav"
-import Navigation from "./components/Navigation/Navigation.jsx";
+import Navigation from "./components/Navigation/Navigation"
+import Calendar from "./components/Calendar/Calendar"
 
 function App() {
     return (
         <div className="App">
             <PlatformNav />
             <Navigation />
+            <Calendar />
         </div>
     )
 }
---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import styles from "./Calendar.module.css"
+
+function Calendar() {
+    return (
+        <div className={styles.Calendar}>
+            <div className={styles.Round}>
+                <div className={styles.Dates}>
+                    <h3>ROUND 1</h3>
+                    <h2>27-28 APRIL 2023</h2>
+                    <h1>FREE PRACTICE 1 COUNTDOWN</h1>
+                </div>
+                <div className={styles.Location}>
+                    <img src="src/assets/Flag_of_Austria.svg.png" alt="Flag of Austria"/>
+                    <h1>SPIELBERG, AUSTRIA</h1>
+                </div>
+            </div>
+            <div className={styles.Countdown}>
+                <div className={styles.CountdownDetail}>
+                    <span className={styles.CountdownTitle}>DAYS</span>
+                    <span className={styles.CountdownTime}>00</span>
+                </div>
+                <div className={styles.CountdownDetail}>
+                    <span className={styles.CountdownTitle}>HOURS</span>
+                    <span className={styles.CountdownTime}>00</span>
+                </div>
+                <div className={styles.CountdownDetail}>
+                    <span className={styles.CountdownTitle}>MINUTES</span>
+                    <span className={styles.CountdownTime}>00</span>
+                </div>
+                <div className={styles.CountdownDetail}>
+                    <span className={styles.CountdownTitle}>SECONDS</span>
+                    <span className={styles.CountdownTime}>00</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Calendar
