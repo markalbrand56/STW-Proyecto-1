04deb4ada3b705e6f69d53c0397c4406cf5fd8cf
Mark Albrand <mark.albrand5@gmail.com>
2023-04-29 17:19:35 -0600

    Dynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,36 @@ import Calendar from "./components/Calendar/Calendar"
 import News from "./components/News/News"
 
 function App() {
+
+    const news = {
+        main: {
+            img: "src/assets/NewsMainImg.webp",
+            category: "INTERVIEW",
+            title: "Inaugural Round of F1 Academy Kicks Off at the Red Bull Ring",
+        },
+        secondary1: {
+            img: "src/assets/NewsImg1.webp",
+            category: "INTERVIEW",
+            title: "Pre-season Testing Concludes With Final Day at the Red Bull Ring",
+        },
+        secondary2: {
+            img: "src/assets/NewsImg2.webp",
+            category: "INTERVIEW",
+            title: "Day 2 of Pre-Season testing completed at Circuit Paul Ricard",
+        },
+        secondary3: {
+            img: "src/assets/NewsImg3.webp",
+            category: "INTERVIEW",
+            title: "Day 1 of testing completed in Le Castellet",
+        },
+    }
+
     return (
         <div className="App">
             <PlatformNav />
             <Navigation />
             <Calendar />
-            <News />
+            <News news={news} />
         </div>
     )
 }
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,39 +1,61 @@
 import React from "react"
+import PropTypes from "prop-types"
 import styles from "./News.module.css"
 
-function News() {
+function News({ news }) {
     return (
         <div className={styles.News}>
             <div className={styles.NewsMainItem}>
-                <img src="src/assets/NewsMainImg.webp" alt="" />
-                <h3>INTERVIEW</h3>
-                <h1>
-                    Inaugural Round of F1 Academy Kicks Off at the Red Bull Ring
-                </h1>
+                <img src={news.main.img} alt="" />
+                <h3>{news.main.category}</h3>
+                <h1>{news.main.title}</h1>
             </div>
             <div className={styles.NewsSecondary}>
                 <div className={styles.NewsSecondaryItem}>
-                    <img src="" alt=""/>
-                    <h3>INTERVIEW</h3>
-                    <h1>
-                        Pre-season Testing Concludes With Final Day at the Red Bull Ring
-                    </h1>
+                    <img src={news.secondary1.img} alt="" />
+                    <h3>{news.secondary1.category}</h3>
+                    <h1>{news.secondary1.title}</h1>
                 </div>
                 <div className={styles.NewsSecondaryItem}>
-                    <img src="" alt="" />
-                    <h3>INTERVIEW</h3>
-                    <h1>
-                        Day 2 of Pre-Season testing completed at Circuit Paul Ricard
-                    </h1>
+                    <img src={news.secondary2.img} alt="" />
+                    <h3>{news.secondary2.category}</h3>
+                    <h1>{news.secondary2.title}</h1>
                 </div>
                 <div className={styles.NewsSecondaryItem}>
-                    <img src="" alt="" />
-                    <h3>INTERVIEW</h3>
-                    <h1>Day 1 of testing completed in Le Castellet</h1>
+                    <img src={news.secondary3.img} alt="" />
+                    <h3>{news.secondary3.category}</h3>
+                    <h1>{news.secondary3.title}</h1>
                 </div>
             </div>
         </div>
     )
+}
+
+const newsShape = PropTypes.shape({
+    main: PropTypes.shape({
+        img: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+    }).isRequired,
+    secondary1: PropTypes.shape({
+        img: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+    }).isRequired,
+    secondary2: PropTypes.shape({
+        img: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+    }).isRequired,
+    secondary3: PropTypes.shape({
+        img: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+    }).isRequired,
+})
+
+News.propTypes = {
+    news: newsShape.isRequired,
 }
 
 export default News
