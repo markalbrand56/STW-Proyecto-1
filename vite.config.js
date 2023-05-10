f724e00ceaca2971756e02a5b3087f5bb16693cb
Mark Albrand <mark.albrand5@gmail.com>
2023-05-09 22:34:29 -0600

    Corrección

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "",
     test: {
         setupFiles: "./src/setupTests.js",
         globals: true,
